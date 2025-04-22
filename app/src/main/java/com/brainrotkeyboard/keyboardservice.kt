99a3c457b22063920ddc6fc14455095550475911
syedshahzaibfaisal <syedshahzaibfaisal007@gmail.com>
2025-04-22 16:29:15 +0500

    Update keyboardservice.kt

--- app/src/main/java/com/brainrotkeyboard/keyboardservice.kt
+++ app/src/main/java/com/brainrotkeyboard/keyboardservice.kt
@@ -1,1 +1,11 @@
+package com.brainrotkeyboard
+
+import android.inputmethodservice.InputMethodService
+import android.view.View
+
+class KeyboardService : InputMethodService() {
+    override fun onCreateInputView(): View {
+        return View(this)
+    }
+}
 
